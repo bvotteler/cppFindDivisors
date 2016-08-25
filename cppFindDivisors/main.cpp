4c39bd18576eeec87a42666b9e0dd68cf2578240
Brendon Votteler <votteler@gmail.com>
2016-08-25 10:02:06 +1000

    Initial Commit

---
+++ cppFindDivisors/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppFindDivisors
+//
+//  Created by Brendon Votteler on 25/08/2016.
+//  Copyright © 2016 Brendon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
